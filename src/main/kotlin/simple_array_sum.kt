11bf4c439a2ff7a08892359edd95cb0c5d228567
Aditprayogo <adit.ihzar@gmail.com>
2020-04-24 02:09:37 +0700

    Initial commit

---
+++ src/main/kotlin/simple_array_sum.kt
@@ -0,0 +1,52 @@
+/**
+Given an array of integers, find the sum of its elements.
+
+For example, if the array , , so return .
+
+Function Description
+
+Complete the simpleArraySum function in the editor below. It must return the sum of the array elements as an integer.
+
+simpleArraySum has the following parameter(s):
+
+ar: an array of integers
+Input Format
+
+The first line contains an integer, , denoting the size of the array.
+The second line contains  space-separated integers representing the array's elements.
+
+Constraints
+
+
+Output Format
+
+Print the sum of the array's elements as a single integer.
+ */
+
+import java.io.*
+import java.math.*
+import java.text.*
+import java.util.*
+import java.util.regex.*
+
+/*
+ * Complete the simpleArraySum function below.
+ */
+fun simpleArraySum(ar: Array<Int>): Int {
+    /*
+     * Write your code here.
+     */
+    return ar.sum()
+}
+
+fun main(args: Array<String>) {
+    val scan = Scanner(System.`in`)
+
+    val arCount = scan.nextLine().trim().toInt()
+
+    val ar = scan.nextLine().split(" ").map{ it.trim().toInt() }.toTypedArray()
+
+    val result = simpleArraySum(ar)
+
+    println(result)
+}
